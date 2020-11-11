78301cea12803878330105e2dc3bb8cf11f7c025
Daniel Sasse <dsasse07@gmail.com>
2020-11-11 21:21:18 +0000

    Done.

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,3 +1,18 @@
+# def find_element_index(array, value_to_find)
+#   # Add your solution here
+#   array.each do |element|
+#     if element == value_to_find
+#       return array.index(element)
+#     else
+#       nil 
+#     end
+#   end
+# end
+
 def find_element_index(array, value_to_find)
-  # Add your solution here
+  if array.include?(value_to_find)
+    array.index(value_to_find)
+  else
+    nil 
+  end
 end
